046a1c07b290dcd5583abb52d896ca4d37582884
Ruben <ruben.hoekema@hotmail.com>
2022-06-10 13:08:52 +0200

    DesktopTimelineComponent is af.

--- vue-src/views/sfeeractiesPagina/components/TimelineComponent.vue
+++ vue-src/views/sfeeractiesPagina/components/TimelineComponent.vue
@@ -1,33 +1,24 @@
 <template>
     <h1 class="d-flex justify-content-center mb-2 mt-2">Tijdlijn</h1>
-    <!-- Desktopview -->
-    <div class="d-none">
-        <div v-for="(item, index) in items" :key="index">
-            <div v-if="index % 2 === 0">
-                <img :src="item.Image" alt="sfeeractiefoto" />
-                <p>{{item.Date}}</p>
-                <p>{{item.Description}}</p>
-                <p>{{item.Date}}</p>
-            </div>
-            <div v-if="index % 2 !== 0">
-                <p>{{item.Date}}</p>
-                <p>{{item.Description}}</p>
-                <img :src="item.Image" alt="sfeeractiefoto"/>
-                <p>{{item.Date}}</p>
-            </div>
-        </div>
+    <!-- Desktop View -->
+
+    <div class="d-none d-lg-block">
+        <DesktopTimelineComponent/>
     </div>
 
     <!-- Mobile View -->
-    <MobileTimelineComponent/>
+    <div class="d-lg-none">
+        <MobileTimelineComponent/>
+    </div>
 </template>
 
 <script>
 import MobileTimelineComponent from "./MobileTimelineComponent.vue";
+import DesktopTimelineComponent from "./DesktopTimelineComponent.vue";
 
 export default {
     name: "TimelineComponent",
-    components: {MobileTimelineComponent}
+    components: {DesktopTimelineComponent, MobileTimelineComponent}
 }
 </script>
 
