7af8c6aa944e7d0429bbf9d983c55dca42ddeaa9
mrgiel <47106654+mrgiel@users.noreply.github.com>
2022-06-14 11:37:53 +0200

    login page

--- vue-src/js/vue-router.js
+++ vue-src/js/vue-router.js
@@ -3,12 +3,14 @@ import HomePage from '@/views/homePagina/HomePage.vue'
 import PageNotFound from "@/views/layout/PageNotFound.vue"
 import OverOnsPage from "@/views/OverOns/OverOnsPage.vue";
 import LidWordenPage from "@/views/LidWorden/LidWordenPage.vue";
+import LogInPage from "@/views/LogIn/LogInPage.vue";
 
 
 let routes = [
     {path: '/', component: HomePage, name: 'HomePage'},
     {path: '/over-ons', component: OverOnsPage, name: 'OverOns'},
     {path: '/lid-worden', component: LidWordenPage, name: 'LidWorden'},
+    {path: '/login', component: LogInPage, name: 'LogIn'}
 
 ];
 
---
+++ vue-src/views/logIn/LogInPage.vue
@@ -0,0 +1,56 @@
+<template>
+    <form @submit.prevent="console.log(form)" style="width: 50%; justify-content: center; margin: auto;">
+        <h1>Login</h1>
+        <div class="row">
+            <div class="col-6">
+                <label class="label">Gebruikersnaam</label>
+                <div class="control">
+                    <input v-model="form.fields.gebruikersnaam.value" type="text" placeholder="Gebruikersnaam" />
+                </div>
+                <!-- Display error message -->
+                <p>
+                    {{ getFieldError(form.fields.gebruikersnaam).join(', ') }}
+                </p>
+            </div>
+            <div class="col-6">
+                <label class="label">Wachtwoord</label>
+                <div class="control">
+                    <input v-model="form.fields.wachtwoord.value" type="password" placeholder="Wachtwoord" />
+                </div>
+                <!-- Display error message -->
+                <p>
+                    {{ getFieldError(form.fields.wachtwoord).join(', ') }}
+                </p>
+            </div>
+        </div>
+    </form>
+</template>
+<script>
+
+import { defineComponent } from 'vue';
+import { getFieldError, getRawFormData, useForm, validateForm } from 'vue3-form';
+
+export default defineComponent({
+    setup() {
+        const form = useForm({
+            gebruikersnaam: { rules: ['required'] },
+            wachtwoord: { rules: ['required'] },
+        });
+
+        const submit = () => {
+            if (!validateForm(form)) {
+                // Form is invalid
+                return;
+            }
+
+            // Form is valid...
+            // proceed with submission.
+            const fields = getRawFormData(form);
+        }
+
+        return { form, getFieldError, submit };
+    },
+});
+</script>
+<style>
+</style>
