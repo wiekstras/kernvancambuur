12ffaad9f6d694b57e84f38ac8bdda017eb8de05
Ruben <ruben.hoekema@hotmail.com>
2022-06-18 15:41:29 +0200

    Added two methods getAddress and insertVolunteer.

--- app/Models/Volunteer.php
+++ app/Models/Volunteer.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Carbon\Carbon as DateTime;
 use Carbon\Traits\Date;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\DB;
 
 /**
  * Class Member
@@ -16,6 +16,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @property string $name
  * @property string $surname
  * @property Date $date_of_birth
+ * @property string $gender
  * @property string $email
  *
  * @property DateTime $created_at
@@ -24,6 +25,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Volunteer extends AbstractModel
 {
     use HasFactory;
+    protected $fillable = ['name', 'surname', 'date_of_birth', 'gender', 'email'];
 
     /**
      * Relationship for volunteer on address.
@@ -33,5 +35,26 @@ class Volunteer extends AbstractModel
     public function address(): BelongsTo
     {
         return $this->belongsTo(Address::class);
+    }
+
+    /**
+     * Get The address of a volunteer by id.
+     *
+     * @param $volunteerId
+     * @return Address|mixed
+     */
+    public function getAddress($volunteerId)
+    {
+        return (new Address)->find($volunteerId);
+    }
+
+    /**
+     * Inserts a volunteer and returns the primary key.
+     * @param $values
+     * @return int
+     */
+    public function insertVolunteer($values): int
+    {
+        return DB::table('volunteers')->insertGetId($values);
     }
 }
