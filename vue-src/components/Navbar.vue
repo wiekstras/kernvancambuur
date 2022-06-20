4c7310e59cf86d6a54dfb1ca95f7f2d953c2b8cc
Gago Khachatryan <gago.khachatryan@student.nhlstenden.com>
2022-06-20 20:30:38 +0200

    fixed conflics

--- vue-src/components/Navbar.vue
+++ vue-src/components/Navbar.vue
@@ -19,36 +19,40 @@
                     <span class="front"></span>
                     <span class="back"></span>
                 </div>
-            </div>
-
+            </form>
+          </div>
         </div>
-        <nav class="navbar navbar-expand-lg">
-            <div class="container">
-                <div class="hamburger">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <img src="../assets/square.png">
-                    </button>
-                </div>
+      </div>
+    </div>
+  </div>
 
+  </div>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
+        <div class="hamburger">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <img src="../assets/square.png">
+          </button>
+        </div>
 
-          <ul class="navbar-nav ul-one mx-auto">
+            <ul class="navbar-nav ul-one mx-auto">
             <a href="/"><img class="logo" src="../assets/logo-kvc.png"></a>
             <a href="/"><img class="logo_text" src="../assets/text-version.png"></a>
           </ul>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ul-one">
+           <ul class="navbar-nav ul-one">
             <li class="nav-item">
                   <a class="nav-link" href="/">Home</a>
               </li>
                 <li class="nav-item">
                   <a class="nav-link" href="/over-ons">Kern van cambuur</a>
               </li>
-                <li class="nav-item">s
+                <li class="nav-item">
                   <a class="nav-link" href="/Sfeeracties">Sfeeracties</a>
               </li>
           </ul>
-
+        
           <ul class="login navbar-nav ul-two ms-auto">
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="/Lid-worden">Lid worden</a>
@@ -67,7 +71,7 @@
             <a class="nav-link" href="/Nieuws">Nieuws</a>
           </li>
         </ul>
-
+      
       </div>
     </div>
   </nav>
@@ -81,122 +85,69 @@
         <div class="col-lg-4 col-md-5 mt-4 text-center buttons">
            <a href="/lid-worden/vrijwilliger-worden"><button type="button"  class="btn btn-outline-primary">Word lid</button></a>
            <a href="/lid-worden/donateur-worden"><button type="button"  class="btn btn-outline-secondary">Steun ons</button></a>
-
-                <ul class="navbar-nav ul-one mx-auto">
-                    <img class="logo" src="../assets/logo-kvc.png">
-                    <img class="logo_text  " src="../assets/text-version.png">
-                </ul>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ul-one">
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/">Home</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/kvc">Kern van cambuur</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/Sfeeracties">Sfeeracties</router-link>
-                        </li>
-                    </ul>
-
-                    <ul class="login navbar-nav ul-two ms-auto">
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/lid-worden">Lid worden</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/contact">Contact</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/wedstrijden">Wedstrijden</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/nieuws">Nieuws</router-link>
-                        </li>
-                    </ul>
-
-                </div>
-            </div>
-        </nav>
-        <div class="container">
-            <div class="htext">
-                <div class="row">
-                    <div class="col-lg-8 col-md-7">
-                        <h2>Supportvereniging</h2>
-                        <h1>SCS Kern van Cambuur</h1>
-                    </div>
-                    <div class="col-lg-4 col-md-5 mt-4 text-center buttons">
-                        <button type="button" class="btn btn-outline-primary">Word lid</button>
-                        <button type="button" class="btn btn-outline-secondary">Steun ons</button>
-                    </div>
-                </div>
-            </div>
         </div>
+      </div>
     </div>
+  </div>
+</div>
 
 </template>
 
 <script>
 export default {
     name: "Navbar",
-
 }
 </script>
 
 <style>
-
 /* General */
 a {
-    text-decoration: none !important;;
+  text-decoration: none !important;;
 }
-
 a:hover {
-    text-decoration: underline!important;
-    color:#ffffff!important;
+  text-decoration: underline!important;
+  color:#ffffff!important;
 }
-
 .container {
     margin-bottom:0%!important;
 }
-
+/* Background img */
+.bg-image {
+  min-height: 390px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  z-index: -1;
+  position: inherit;
+}
 /* TopBar */
-
 .topbar {
-    padding: 13px 0px;
+ padding: 13px 0px;
 }
-
 .search {
-    text-align: right;
+  text-align: right;
 }
-
 .social {
-    margin-top:9px;
+  margin-top:9px;
 }
-
 .social span {
-    color: #fff;
-    margin-left: 10px;
-    text-shadow: 0px 1px 2px black;
-    font-size: 14px;
+  color: #fff;
+  margin-left: 10px;
+  text-shadow: 0px 1px 2px black;
+  font-size: 14px;
 }
-
 .social img {
-    width:20px;
-    padding: 10px 0px;
-    margin-left: 10px;
+  width:20px;
+  padding: 10px 0px;
+  margin-left: 10px;
 }
-
 /* Navbar */
-
 nav {
-    border-top: 3px solid #FAE300;
-    border-bottom: 3px solid #246DB9;
+  border-top: 3px solid #FAE300;
+  border-bottom: 3px solid #246DB9;
 }
-
 .navbar-collapse {
-    z-index: 1;
+  z-index: 1;
 }
-
 nav .logo {
     position: absolute;
     top: 2px;
@@ -206,15 +157,12 @@ nav .logo {
     height: 225px;
     cursor: pointer;
 }
-
 .navbar-toggler {
-    padding:0px !important;
+  padding:0px !important;
 }
-
 .navbar-toggler:focus {
-    box-shadow:none!important;
+  box-shadow:none!important;
 }
-
 nav .navbar-nav .nav-link {
     font-weight: 500;
     font-size: 16px;
@@ -222,9 +170,7 @@ nav .navbar-nav .nav-link {
     color:#ffffff;
     text-shadow:0px 1px 2px black;
 }
-
 /* Search button */
-
 .expSearchFrom {
     border: 3px solid #246DB9;
     border-radius: 20px;
@@ -233,7 +179,6 @@ nav .navbar-nav .nav-link {
     position: relative;
     margin-top: 8px;
 }
-
 .expSearchBox input#field {
     cursor: pointer;
     border: none;
@@ -249,14 +194,12 @@ nav .navbar-nav .nav-link {
     z-index: 2;
     color: #000;
 }
-
 .expSearchBox input#field:focus {
     width: 100px;
     outline: none;
     font-size: 12px;
     margin-right: 30px;
 }
-
 .expSearchBox .close {
     position: absolute;
     width: 30px;
@@ -270,7 +213,6 @@ nav .navbar-nav .nav-link {
     cursor: pointer;
     z-index: 1;
 }
-
 .expSearchBox span {
     background: #246DB9 none repeat scroll 0 0;
     border-radius: 3px;
@@ -289,18 +231,15 @@ nav .navbar-nav .nav-link {
     -o-transform: rotate(45deg);
     transform: rotate(45deg);
 }
-
 .expSearchBox .back {
     top: 12px;
     right: 5px;
     opacity: 0;
 }
-
 .expSearchBox input#field:focus + .close span {
     top: 12px;
     right: 5px;
 }
-
 .expSearchBox input#field:focus + .close span.back {
     -webkit-transition: .5s .7s ease-in-out;
     -moz-transition: .5s .7s ease-in-out;
@@ -312,99 +251,73 @@ nav .navbar-nav .nav-link {
     transform: rotate(-45deg);
     opacity: 1;
 }
-
 /* Heading text + buttons */
 .htext {
-    color:#fff;
-    padding-top:58px;
+  color:#fff;
+  padding-top:58px;
 }
-
 .htext h2 {
-    color:#FAE300!important;
-    text-shadow: 0px 1px 2px black;
+  color:#FAE300!important;
+  text-shadow: 0px 1px 2px black;
 }
-
 .htext h1 {
-    text-shadow: 0px 1px 2px black;
+  text-shadow: 0px 1px 2px black;
 }
-
 .btn {
-    border-radius: 1px;
-    text-transform: uppercase;
-    padding: 5px 21px;
-    font-size:20px;
+  border-radius: 1px;
+  text-transform: uppercase;
+  padding: 5px 21px;
+  font-size:20px;
 }
-
 .btn-outline-primary {
     margin-right: 20px;
     color: #fff;
     border-color: #FAE300;
 }
-
 .btn-outline-secondary {
     color: #ffffff;
     border-color: #246DB9;
 }
-
 .btn-outline-secondary:hover {
     color: #000;
     background-color: #246DB9;
 }
-
 .btn-outline-primary:hover {
     background-color: #FAE300;
     color: #000;
 }
-
 /* Media queries */
-
-@media all and (min-width: 992px) {
-    .navbar .nav-item .dropdown-menu{ display: none; }
-    .navbar .nav-item:hover .nav-link{   }
-    .navbar .nav-item:hover .dropdown-menu{ display: block; }
-    .navbar .nav-item .dropdown-menu{ margin-top:0; }
-}
-
 @media only screen and (max-width: 992px) {
-    .social span {
-        display:none;
-    }
-
-    .loc, .email {
-        margin-left:21px!important;
-    }
-
-    .logo {
-        display:none;
-    }
-
-    .logo_text {
-        display: block !important;
-    }
+ .social span {
+  display:none;
+ }
+ .loc, .email {
+  margin-left:21px!important;
+ }
+  .logo {
+    display:none;
+  }
+ .logo_text {
+  display: block !important;
+ }
 }
-
 @media only screen and (max-width: 17000px) {
-    .logo_text {
-        display:none;
-    }
+   .logo_text {
+    display:none;
+  }
 }
-
 @media only screen and (max-width: 767px) {
-    nav .logo {
-        margin-left: -133px;
-        width: 270px;
-        height: 176px;
-    }
-
-    .htext {
-        padding-top:29px;
-        text-align: center;
-    }
-
-    .htext .buttons {
-        margin-top:13px!important;
-    }
-
+  nav .logo {
+      margin-left: -133px;
+      width: 270px;
+      height: 176px; 
+  }
+  .htext {
+    padding-top:29px;
+    text-align: center;
+  }
+  .htext .buttons {
+    margin-top:13px!important;
+  }
 }
-
 </style>
