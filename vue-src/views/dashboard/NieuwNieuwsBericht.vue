217b7c121732415f0003c085a9e6e1feacdc8977
Stan <Wiekstras@gmail.com>
2022-07-02 20:57:55 +0200

    Change button name

--- vue-src/views/dashboard/NieuwNieuwsBericht.vue
+++ vue-src/views/dashboard/NieuwNieuwsBericht.vue
@@ -40,7 +40,7 @@
                     type="submit"
                     class="inline-flex justify-center mx-2 py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-primary hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                    Maak sfeeractie
+                    Maak nieuwsbericht
                 </button>
             </div>
         </form>
